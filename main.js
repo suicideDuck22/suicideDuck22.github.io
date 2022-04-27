149919ace9d6ace25971aa8752cd6396bd720ef9
RenanDrnls <rdorneles64@gmail.com>
2022-04-27 00:10:47 -0300

    Added navigator and style on scrollbar

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 function changeProjectLinks(){
-    for(let li of projects.children){
+    for(let li of projectsNames.children){
         const project = li.getAttribute('class');
         li.children[0].href = `https://github.com/suicideDuck22/${project}`
     }
 }
 
-changeProjectLinks()
+changeProjectLinks();
+
+const userSocialMedias = {
+    github: 'suicideDuck22',
+    instagram: 'renas.js/',
+    facebook: 'profile.php?id=100010200602377'
+}
+
+function changeSocialLinks(){
+    for(let li of socialLink.children){
+        const social = li.getAttribute('class');
+        li.children[0].href = `https://www.${social}.com.br/${userSocialMedias[social]}`
+    }
+}
+
+changeSocialLinks();
